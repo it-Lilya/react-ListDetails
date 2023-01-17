563ecd538ba2b265ac987aacb858c73a6cb30057
It_Lilya <ihneva@bk.ru>
2023-01-18 01:27:56 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { List } from './components/List/List';
 import { Details } from './components/Details/Details';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 
 function App() {
   const [listItem, setListItem] = useState([]);
-  const [info, setInfo] = useState();
+  const [info, setInfo] = useState(null);
+  const [user, setUser] = useState({});
+
 
   const componentDidMount = () => {
     fetch('https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json')
@@ -19,11 +21,38 @@ function App() {
     setInfo({id: target.id, name: target.textContent});
   }
 
+
+  useEffect(() => {
+    if (info !== null) {
+      let url = `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => setUser(data));
+    } 
+  }, [info])
+  
+// console.log(user.id)
+  // const isInitialMount = useRef(true);
+  // useEffect(() => {
+  //     if (isInitialMount.current) {
+  //       isInitialMount.current = false;
+  //     } else {
+  //       if (info !== null) {
+  //         let url = `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`;
+  //         console.log(url)
+  //         fetch(url)
+  //           .then((response) => response.json())
+  //           .then((data) => setUser(data));
+  //         isInitialMount.current = true;
+  //       }
+  //     }
+  // });
+
   return (
     <div className="App">
-      <List open={handleItemClick} data={listItem} />
-      {info ? <Details info={info} /> : null}
-      {/* <Details info={info} /> */}
+     <List open={handleItemClick} data={listItem} />
+      {info == null ? "loading..." : <Details info={user} />}
+      {/* {info !== null ? 'Loading...' : <Details info={user} />} */}
     </div>
   );
 }
--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,27 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import './Details.css';
 
-export function Details({info}) {
-  const [user, setUser] = useState({});
-
-  const handle = () => {
-    let url = `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`;
-      fetch(url)
-        .then(response => response.json())
-        .then(data => setUser(data))
-  }
-  useEffect(handle, [info.id]);
-  console.log(user.details)
-  
+export function Details({info}) { 
   return (
     <div className="details">
-      <img src={`${user.avatar}?${Math.random()}`} alt='...' />
-        {/* <div className="container">
-          <h3 className="element">{user.name}</h3>
-          <p className="element">City: {user.details.city}</p>
-          <p className="element">Company: {user.details.company}</p>
-          <p className="element">Position: {user.details.position}</p>
-        </div> */}
+      <img src={`${info.avatar}?${Math.random()}`} alt='...' />
+        <div className="container">
+          <h3 className="element">{info.name}</h3>
+          <p className="element">City: {info.details.city}</p>
+          <p className="element">Company: {info.details.company}</p>
+          <p className="element">Position: {info.details.position}</p>
+        </div>
     </div>
   );
 }
